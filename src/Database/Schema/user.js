4a025d7007b1e07bb11e72f90f47bf80d003966a
saikrish1787 <83500318+saikrish1787@users.noreply.github.com>
2023-10-24 01:51:05 +0530

    First commit

---
+++ src/Database/Schema/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: {
+    type: Number,
+    min: 1,
+    validate: {
+      validator: (v) => v > 18,
+      message: "User should have an age above 18",
+    },
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
